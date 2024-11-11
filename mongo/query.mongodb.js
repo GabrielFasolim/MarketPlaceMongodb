eef5c665463c05b60148cb3641f5a8cd9b7700f3
nsx07 <felipeaugustodluz@hotmail.com>
2024-11-11 15:05:50 -0300

    fix insert query

--- mongo/query.mongodb.js
+++ mongo/query.mongodb.js
@@ -1,15 +1,14 @@
 /* global use, db */
 use("MarketPlaceDBEV");
 
-/**
 // 3. Consultas:
 // Escreva uma consulta para encontrar todos os produtos em uma categoria específica.
-const produto = db.produto.findOne({ "categoria.nome": "Camisetas" });
+const produto = db.produto.find({ "categoria.nome": "Camisetas" }).toArray();
 
 console.log(produto);
 
 // Escreva uma consulta para encontrar todas as avaliações de um produto específico.
-const avaliacao = db.avaliacao.find({ produto: produto._id });
+const avaliacao = db.avaliacao.find({ produto: produto[0]._id });
 
 console.log(avaliacao.toArray());
 
@@ -20,8 +19,6 @@ const produtos = db.produto
   .find({ quantidade: { $gt: 1 } }, { _id: 1, descricao: 1, preco: 1 })
   .limit(2)
   .toArray();
-
-console.log(usuario, produtos);
 
 const carrinho = produtos.map((produto) => ({
   produto: produto._id,
@@ -48,209 +45,165 @@ carrinho.forEach((item) =>
     }
   )
 );
- */
 
-/**
-// 4 - Indices
-
-// usuarios
-db.usuarios.createIndex({ email: 1 }, { unique: true });
-
-// categorias
-db.categorias.createIndex({ nome: 1 });
-
-// produtos
-db.produtos.createIndex({ nome: 1 });
-db.produtos.createIndex({ categoria: 1 });
-
-// avaliacoes
-db.avaliacoes.createIndex({ produto: 1 });
-db.avaliacoes.createIndex({ usuario: 1 });
-
-// transacoes
-db.transacoes.createIndex({ produto: 1 });
-db.transacoes.createIndex({ usuario: 1 });
- */
-
-/**
 // 5. Agregações:
 
 //- Escreva uma consulta de agregação para encontrar a média de avaliações para cada produto.
-db.avaliacao.aggregate([
-  {
-    $group: {
-      _id: "$produto",
-      media: { $avg: "$nota" },
-    },
-  },
-]);
+console.log(
+  db.avaliacao
+    .aggregate([
+      {
+        $lookup: {
+          from: "produto",
+          localField: "produto",
+          foreignField: "_id",
+          as: "produto",
+        },
+      },
+      {
+        $unwind: "$produto",
+      },
+      {
+        $group: {
+          _id: "$produto._id",
+          nome: { $first: "$produto.nome" },
+          media: { $avg: "$nota" },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          nome: 1,
+          media: 1,
+        },
+      },
+    ])
+    .toArray()
+);
 
 //- Escreva uma consulta de agregação para encontrar o total de vendas para cada categoria.
-db.transacao.aggregate([
-  {
-    $unwind: "$produtos",
-  },
-  {
-    $lookup: {
-      from: "produto",
-      localField: "produtos.produto",
-      foreignField: "_id",
-      as: "produto_info",
-    },
-  },
-  {
-    $unwind: "$produto_info",
-  },
-  {
-    $group: {
-      _id: "$produto_info.categoria.nome",
-      totalVendas: {
-        $sum: "$produtos.total",
+console.log(
+  db.transacao
+    .aggregate([
+      {
+        $unwind: "$produtos",
       },
-    },
-  },
-]);
- */
+      {
+        $lookup: {
+          from: "produto",
+          localField: "produtos.produto",
+          foreignField: "_id",
+          as: "produto_info",
+        },
+      },
+      {
+        $unwind: "$produto_info",
+      },
+      {
+        $group: {
+          _id: "$produto_info.categoria.nome",
+          totalVendas: {
+            $sum: "$produtos.total",
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          categoria: "$_id",
+          totalVendas: 1,
+        },
+      },
+    ])
+    .toArray()
+);
 
 // Sprint 2
 
-// const promocaoProduto = db.promocao
-//   .aggregate([
-//     {
-//       $lookup: {
-//         from: "produto",
-//         localField: "produto",
-//         foreignField: "_id",
-//         as: "produtos",
-//       },
-//     },
-//     {
-//       $unwind: "$produtos",
-//     },
-//     {
-//       $project: {
-//         promocao: "$_id",
-//         produto: "$produtos._id",
-//         nome: "$produtos.nome",
-//         preco: "$produtos.preco",
-//         preco_promocional: {
-//           $subtract: [
-//             "$produtos.preco",
-//             {
-//               $divide: [
-//                 { $multiply: ["$produtos.preco", "$descontoPercentual"] },
-//                 100,
-//               ],
-//             },
-//           ],
-//         },
-//       },
-//     },
-//   ])
-//   .toArray();
-
-// const usuario = db.usuario.findOne({ nome: /^Maria/ }, { _id: 1, nome: 1 });
-
-// const carrinho = promocaoProduto.map((produto) => ({
-//   produto: produto.produto,
-//   quantidade: 1,
-//   preco: Double(produto.preco_promocional),
-//   total: Double(produto.preco_promocional * 1),
-//   promocao: {
-//     pid: produto.promocao,
-//     precoOriginal: Double(produto.preco),
-//     precoDesconto: Double(produto.preco_promocional),
-//   },
-// }));
-
-// db.transacao.insertOne({
-//   _id: ObjectId("6485e5f9c4d60d43e7b9c2a5"),
-//   usuario: usuario._id,
-//   produtos: carrinho,
-//   data: new Date(),
-//   status: "pendente",
-//   valor_total: Double(carrinho.reduce((acc, curr) => acc + curr.total, 0)),
-//   quantidade_total: carrinho.reduce((acc, curr) => acc + curr.quantidade, 0),
-// });
-
-// carrinho.forEach((item) =>
-//   db.produto.updateOne(
-//     { _id: item.produto },
-//     {
-//       $inc: { quantidade: -item.quantidade },
-//     }
-//   )
-// );
-
-// db.pontuacao.updateOne(
-//   { usuario: usuario._id },
-//   {
-//     $inc: {
-//       pontos: carrinho.reduce((acc, curr) => acc + curr.total, 0),
-//     },
-//     $push: {
-//       operacoes: {
-//         data: new Date(),
-//         operation: "credit",
-//         transacao: ObjectId("6485e5f9c4d60d43e7b9c2a5"),
-//         pontos: carrinho.reduce((acc, curr) => acc + curr.total, 0),
-//       },
-//     },
-//   },
-//   { upsert: true }
-// );
-
-db.usuario.createIndex({ "endereco.localizacao": "2dsphere" });
-db.produto.createIndex({ localizacao_vendedor: "2dsphere" });
-
-// Atualizar localização dos vendedores
-db.usuario.updateOne(
-  { _id: ObjectId("6485e5f9c4d60d43e7b9c0e4") }, // Ana Costa
-  {
-    $set: {
-      "endereco.localizacao": {
-        type: "Point",
-        coordinates: [-38.4713, -12.9777], // Coordenadas em Salvador
+const promocaoProduto = db.promocao
+  .aggregate([
+    {
+      $lookup: {
+        from: "produto",
+        localField: "produto",
+        foreignField: "_id",
+        as: "produtos",
       },
     },
-  }
+    {
+      $unwind: "$produtos",
+    },
+    {
+      $project: {
+        promocao: "$_id",
+        produto: "$produtos._id",
+        nome: "$produtos.nome",
+        preco: "$produtos.preco",
+        preco_promocional: {
+          $subtract: [
+            "$produtos.preco",
+            {
+              $divide: [
+                { $multiply: ["$produtos.preco", "$descontoPercentual"] },
+                100,
+              ],
+            },
+          ],
+        },
+      },
+    },
+  ])
+  .toArray();
+
+const usuario1 = db.usuario.findOne({ nome: /^Maria/ }, { _id: 1, nome: 1 });
+
+const carrinho1 = promocaoProduto.map((produto) => ({
+  produto: produto.produto,
+  quantidade: 1,
+  preco: Double(produto.preco_promocional),
+  total: Double(produto.preco_promocional * 1),
+  promocao: {
+    pid: produto.promocao,
+    precoOriginal: Double(produto.preco),
+    precoDesconto: Double(produto.preco_promocional),
+  },
+}));
+
+db.transacao.insertOne({
+  _id: ObjectId("6485e5f9c4d60d43e7b9c2a5"),
+  usuario: usuario1._id,
+  produtos: carrinho1,
+  data: new Date(),
+  status: "pendente",
+  valor_total: Double(carrinho.reduce((acc, curr) => acc + curr.total, 0)),
+  quantidade_total: carrinho.reduce((acc, curr) => acc + curr.quantidade, 0),
+});
+
+carrinho1.forEach((item) =>
+  db.produto.updateOne(
+    { _id: item.produto },
+    {
+      $inc: { quantidade: -item.quantidade },
+    }
+  )
 );
 
-db.usuario.updateOne(
-  { _id: ObjectId("6485e5f9c4d60d43e7b9c0e5") }, // Pedro Lima
+db.pontuacao.updateOne(
+  { usuario: usuario1._id },
   {
-    $set: {
-      "endereco.localizacao": {
-        type: "Point",
-        coordinates: [-38.3213, -12.8877], // Coordenadas em Lauro de Freitas
+    $inc: {
+      pontos: carrinho1.reduce((acc, curr) => acc + curr.total, 0),
+    },
+    $push: {
+      operacoes: {
+        data: new Date(),
+        operation: "credit",
+        transacao: ObjectId("6485e5f9c4d60d43e7b9c2a5"),
+        pontos: carrinho1.reduce((acc, curr) => acc + curr.total, 0),
       },
     },
-  }
-);
-
-// Atualizar produtos com a localização dos vendedores
-db.produto.updateMany(
-  { vendedor: ObjectId("6485e5f9c4d60d43e7b9c0e4") },
-  {
-    $set: {
-      localizacao_vendedor: {
-        type: "Point",
-        coordinates: [-38.4713, -12.9777],
-      },
-    },
-  }
-);
-
-db.produto.updateMany(
-  { vendedor: ObjectId("6485e5f9c4d60d43e7b9c0e5") },
-  {
-    $set: {
-      localizacao_vendedor: {
-        type: "Point",
-        coordinates: [-38.3213, -12.8877],
-      },
-    },
-  }
+  },
+  { upsert: true }
 );
 
 db.produto.find({
@@ -258,14 +211,13 @@ db.produto.find({
     $near: {
       $geometry: {
         type: "Point",
-        coordinates: [-38.4713, -12.9777],
+        coordinates: [-38.3213, -12.8877],
       },
       $maxDistance: 10 * 1000, // Converter km para metros
     },
   },
 });
 
-// Agregação para calcular média de distância em transações concluídas
 db.transacao.aggregate([
   {
     $match: {
@@ -311,22 +263,58 @@ db.transacao.aggregate([
   {
     $project: {
       distancia: {
-        $divide: [
-          {
-            $geoDistance: {
-              from: "$comprador.endereco.localizacao",
-              to: "$vendedor.endereco.localizacao",
-              spherical: true,
+        $sqrt: {
+          $add: [
+            {
+              $pow: [
+                {
+                  $subtract: [
+                    {
+                      $arrayElemAt: [
+                        "$comprador.endereco.localizacao.coordinates",
+                        0,
+                      ],
+                    },
+                    {
+                      $arrayElemAt: [
+                        "$vendedor.endereco.localizacao.coordinates",
+                        0,
+                      ],
+                    },
+                  ],
+                },
+                2,
+              ],
             },
-          },
-          1000,
-        ], // Converter metros para quilômetros
+            {
+              $pow: [
+                {
+                  $subtract: [
+                    {
+                      $arrayElemAt: [
+                        "$comprador.endereco.localizacao.coordinates",
+                        1,
+                      ],
+                    },
+                    {
+                      $arrayElemAt: [
+                        "$vendedor.endereco.localizacao.coordinates",
+                        1,
+                      ],
+                    },
+                  ],
+                },
+                2,
+              ],
+            },
+          ],
+        },
       },
     },
   },
   {
     $group: {
-      _id: null,
+      _id: 0,
       distanciaMedia: { $avg: "$distancia" },
     },
   },
